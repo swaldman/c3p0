237e1fd5a201c54cd3d2e42bb3bef19c885d4e21
Steve Waldman <swaldman@mchange.com>
2013-03-09 08:14:18 +0000

    Added parentLogger to list of properties excluded from ComboPooledDataSource.toString to prevent an introspection Exception.

--- src/java/com/mchange/v2/c3p0/ComboPooledDataSource.java
+++ src/java/com/mchange/v2/c3p0/ComboPooledDataSource.java
@@ -70,6 +70,7 @@ public final class ComboPooledDataSource extends AbstractPoolBackedDataSource im
                     "numUnclosedOrphanedConnectionsDefaultUser",
                     "numUserPools",
                     "effectivePropertyCycleDefaultUser",
+                    "parentLogger",
                     "startTimeMillisDefaultUser",
                     "statementCacheNumCheckedOutDefaultUser",
                     "statementCacheNumCheckedOutStatementsAllUsers",
