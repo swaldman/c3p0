9ba5276e1dd86c7fa9ff31300435d0543c36a20d
Steve Waldman <swaldman@mchange.com>
2013-03-20 08:17:13 +0000

    Relicensed c3p0, dual LGPL 2.1 / EPL 1.0

--- src/java/com/mchange/v2/c3p0/jboss/C3P0PooledDataSourceMBean.java
+++ src/java/com/mchange/v2/c3p0/jboss/C3P0PooledDataSourceMBean.java
@@ -1,25 +1,37 @@
 /*
- * Distributed as part of c3p0 v.0.9.2
+ * Distributed as part of c3p0 v.0.9.2.1
  *
- * Copyright (C) 2012 Machinery For Change, Inc.
+ * Copyright (C) 2013 Machinery For Change, Inc.
  *
  * Author: Steve Waldman <swaldman@mchange.com>
  *
  * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 2.1, as 
- * published by the Free Software Foundation.
+ * it under the terms of EITHER:
+ *
+ *     1) The GNU Lesser General Public License (LGPL), version 2.1, as 
+ *        published by the Free Software Foundation
+ *
+ * OR
+ *
+ *     2) The Eclipse Public License (EPL), version 1.0
+ *
+ * You may choose which license to accept if you wish to redistribute
+ * or modify this work. You may offer derivatives of this work
+ * under the license you have chosen, or you may provide the same
+ * choice of license which you have been offered here.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; see the file LICENSE.  If not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received copies of both LGPL v2.1 and EPL v1.0
+ * along with this software; see the files LICENSE-EPL and LICENSE-LGPL.
+ * If not, the text of these licenses are currently available at
+ *
+ * LGPL v2.1: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *  EPL v1.0: http://www.eclipse.org/org/documents/epl-v10.php 
+ * 
  */
-
 
 package com.mchange.v2.c3p0.jboss;
 
