0f4b40aac6bdbaed6f819f291a9c54e782695e1c
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2014-03-03 10:48:55 +0100

    remove concurrentskyplistset import (as not working with mysql driver)

--- src/java/com/mchange/v2/c3p0/stmt/GooGooStatementCache.java
+++ src/java/com/mchange/v2/c3p0/stmt/GooGooStatementCache.java
@@ -49,7 +49,6 @@ import java.io.StringWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListSet;
 
 import com.mchange.v2.io.IndentedWriter;
 
