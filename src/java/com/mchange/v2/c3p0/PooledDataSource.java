5d476cbf1594beb67ca054806069005e24737306
Steve Waldman <swaldman@mchange.com>
2014-10-04 19:59:24 -0700

    Made PooledDataSource AutoCloseable, so that try with resources can be used to clean up after them.

--- src/java/com/mchange/v2/c3p0/PooledDataSource.java
+++ src/java/com/mchange/v2/c3p0/PooledDataSource.java
@@ -122,7 +122,7 @@ import java.util.Map;
  *    silently excluded from the pool, and which remain unclosed by the client application.</li>
  *  </ol>
  */
-public interface PooledDataSource extends DataSource
+public interface PooledDataSource extends DataSource, AutoCloseable
 {
     public String getIdentityToken();
     public String getDataSourceName();
