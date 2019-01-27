c422ead14de1ca530b72d5e0515880b7ed27df1e
Steve Waldman <swaldman@mchange.com>
2019-01-27 00:10:02 -0800

    Update source headers for v.0.9.5.3.

--- src/java/com/mchange/v2/resourcepool/BasicResourcePool.java
+++ src/java/com/mchange/v2/resourcepool/BasicResourcePool.java
@@ -1,14 +1,14 @@
 /*
- * Distributed as part of c3p0 v.0.9.5.2
+ * Distributed as part of c3p0 v.0.9.5.3
  *
- * Copyright (C) 2015 Machinery For Change, Inc.
+ * Copyright (C) 2018 Machinery For Change, Inc.
  *
  * Author: Steve Waldman <swaldman@mchange.com>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of EITHER:
  *
- *     1) The GNU Lesser General Public License (LGPL), version 2.1, as
+ *     1) The GNU Lesser General Public License (LGPL), version 2.1, as 
  *        published by the Free Software Foundation
  *
  * OR
@@ -29,8 +29,8 @@
  * If not, the text of these licenses are currently available at
  *
  * LGPL v2.1: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- *  EPL v1.0: http://www.eclipse.org/org/documents/epl-v10.php
- *
+ *  EPL v1.0: http://www.eclipse.org/org/documents/epl-v10.php 
+ * 
  */
 
 package com.mchange.v2.resourcepool;
